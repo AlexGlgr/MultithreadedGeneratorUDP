e4b49e22007119df036deb3d3b7ba32f33c68c75
AlexGlgr <arrarrat65@gmail.com>
2025-09-01 15:05:53 +0400

    fix: third iteration with TPC control

--- js/server/process_based/server_cluster.js
+++ js/server/process_based/server_cluster.js
@@ -5,165 +5,113 @@ import {env} from 'node:process';
 import { execSync } from 'node:child_process';
 import dgram  from 'dgram';
 import net from 'node:net';
-import { readFile } from 'node:fs';
 
 // Конфигурация по умолчанию
-const DEFAULT_PORT_BASE = 40000;
-const DEFAULT_CORE = 0;
-const DEFAULT_SOCKET_SIZE = 1;
-const DEFAULT_IP = '0.0.0.0';
+const DEFAULT_TCP_PORT = 9999;
+const DEFAULT_TCP_IP = '10.120.100.51';
+
 
 // Парсинг аргументов командной строки
 const args = minimist(process.argv.slice(2), {
     alias: {
-        p: 'portBase',
-        o: 'portCount',
-        r: 'roundRobin',
-        c: 'baseCPU',
-        s: 'sockets',
-        i: 'baseIP',
-        m: 'mode',
-        q: 'stock',
-        f: 'file'
+        p: 'port',
+        i: 'ip',
     },
     default: {
-        portBase: DEFAULT_PORT_BASE,
-        portCount: 1,
-        baseCPU: DEFAULT_CORE,
-        sockets: DEFAULT_SOCKET_SIZE,
-        baseIP: DEFAULT_IP,
-        roundRobin: 0,
-        file: './test.json'
+        port: DEFAULT_TCP_PORT,
+        ip: DEFAULT_TCP_IP
+       
     }
 });
 
-const sockets = parseInt(args.sockets);
-const portBase = parseInt(args.portBase);
-const portCount = parseInt(args.portCount);
-const baseCPU = parseInt(args.baseCPU);
-const baseIP = args.baseIP;
-const singularMode = (args.mode === undefined ? false : true);
-const stockIP = (args.stock === undefined ? false : true);
-const roundRobin = parseInt(args.roundRobin) === 0 ? 0 : 1;
-const file = args.file;
+const tcp_port = parseInt(args.port);
+if (isNaN(tcp_port)) { console.error('Invalid TCP port, using default value', DEFAULT_TCP_PORT);
+    tcp_port = DEFAULT_TCP_PORT;
+}
 
-const octetStrings = baseIP.split('.');
-const octetIntegers = octetStrings.map(octet => parseInt(octet, 10));
+const tcp_ip = args.ip;
+if (net.isIP(tcp_ip) === 0) { console.error('Invalid IP address, using default value', DEFAULT_TCP_IP);
+    tcp_ip = DEFAULT_TCP_IP;
+}
 
-if (isNaN(portBase)) throw new Error('Invalid port base');
-if (isNaN(baseCPU)) throw new Error('Invalid core ID');
+let workers = [];
+const speedData = [];
+let _env;
+let interval;
+let gl_recieved_packets = 0;
+let gl_send_packets = 0;
+let gl_max_speed = 0;
 
 if (cluster.isPrimary) {
-    cluster.schedulingPolicy = roundRobin === 0 ? cluster.SCHED_NONE : cluster.SCHED_RR;
-    let gl_recieved_packets = 0;
-    let gl_send_packets = 0;
-    let gl_max_speed = 0;
-    let countDown = 0;
-    const workers = [];
-    const speedData = [];
-    let _env = Object.assign({}, process.env);
+    _env = Object.assign({}, process.env);
 
-    readFile(file, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        }
-        else {
-            const jsonData = JSON.parse(data);
-            console.log(jsonData);
-        }
-    });
-
-    for (let i = 0; i < sockets*portCount; i++) {
-        _env.port = Math.floor(portBase + (i / sockets));
-        _env.baseCPU = baseCPU + i;
-        _env.ip = octetIntegers.join('.');
-        _env.mode = singularMode;
-        const worker = cluster.fork(_env);
-        workers.push(worker);
-        if (stockIP){
-            octetIntegers[3]++;
-        }
-        speedData.push({port: _env.port, speed: 0, pps: 0, overall: 0});
-        worker.on('message', (msg) => {
-            speedData[i].speed = msg.speed;
-            speedData[i].pps = msg.pps;
-            speedData[i].overall += msg.pps;
-        });
-    }
-    if (os.type() == 'Linux') {
-        const { pid } = process;
-        const cpu = baseCPU;
-        execSync(`taskset -cp ${cpu} ${pid}`);
-        console.log(`Main cluster with pid ${process.pid} running on Core ${cpu}`);
-    }
+    const client = new net.Socket(); 
 
     const tcp_server = net.createServer((socket) => {
         socket.on('data', (data) => {
-            gl_send_packets += JSON.parse(data.toString()).packets_send;
+            const msg = JSON.parse(data.toString());
+            switch (msg.com) {
+                case 'register':
+                    if (isNaN(parseInt(msg.port)) || net.isIP(msg.ip) === 0) {
+                        console.error(`Cannot connect via recieved data! Port:${data.port}, IP: ${data.ip}`);
+                        break;
+                    }
+                    client.connect(msg.port, msg.ip, () => {
+                        console.log('Connected to server ', msg.ip);
+
+                        // Send data to the server
+                        client.write(JSON.stringify({com: 'registered', name: 'cluster_server', status: 'running'}));
+                    });                    
+                    break;
+                case 'packets':
+                    gl_send_packets += msg.packets_send;
+                    break;
+                case 'drypackets':
+                    const packets = msg.packets_send;
+                    gl_send_packets += packets;
+                    console.log(`Recieved ${packets} packets out of (${gl_send_packets} total)`);
+                    break;
+                case 'start':
+                    start(msg.config);
+                    client.write(JSON.stringify({com: 'started', name: 'cluster_server', status: 'listenning'}));
+                    break;
+                case 'stop':
+                    console.log(`Test is over. Max speed: ${gl_max_speed.toFixed(2)} Gbit/s | Send packets: ${gl_send_packets == 0 ? 'No data' : gl_send_packets} | Received packets: ${gl_recieved_packets} | `
+                            + `Loss percentage: ${gl_send_packets == 0 ? 'No data' : `${((1 - (gl_recieved_packets/gl_send_packets))*100).toFixed(2)}%`}`);
+                    gl_send_packets = 0;
+                    gl_recieved_packets = 0;
+                    gl_max_speed = 0;
+                    speedData.forEach((record) => {
+                        record.speed = 0;
+                        record.pps = 0;
+                        record.overall = 0;
+                    });
+                    workers.forEach((worker) => {
+                        worker.kill();
+                    });
+                    clearInterval(interval);
+                    client.write(JSON.stringify({com: 'stopped', name: 'cluster_server', status: 'running'}));
+                    break;
+                case 'roundrobin':
+                    cluster.schedulingPolicy = data.rr === cluster.SCHED_RR ? cluster.SCHED_RR : cluster.SCHED_NONE;
+                    console.log('Schedule policy set to ', cluster.schedulingPolicy);
+                    break;
+                default:
+                    break;
+            }
+            //gl_send_packets += JSON.parse(data.toString()).packets_send;
         });
     });
-    
-    tcp_server.listen(9999, () => {
+            
+    tcp_server.listen(tcp_port, tcp_ip, () => {
         console.log('TCP connection on ', tcp_server.address());
     });
 
-    setInterval(() => {
-        let overall_speed = 0;
-        let overall_packet_count = 0;
-        let overall_pps = 0;
-        
-        
-        for (let group_idx = 0; group_idx < portCount; group_idx++) {
-            let group_speed = 0;
-            let group_packet_count = 0;
-            let group_port = 0;
-            let group_pps = 0;
-
-            for (let cluster_idx = 0; cluster_idx < sockets; cluster_idx++) {
-
-                let spData_key = (sockets*group_idx)+cluster_idx;
-
-                group_speed += speedData[spData_key].speed;
-                group_packet_count += speedData[spData_key].overall;
-                group_port = speedData[spData_key].port;
-                group_pps += speedData[spData_key].pps;               
-            }
-            if (group_speed > 0) {
-                if (group_idx == 0) console.log('\n\n');
-
-                overall_speed += group_speed;
-                overall_pps += group_pps;
-                overall_packet_count += group_packet_count;
-
-                console.log(`[SERVER PORT:${group_port}] Speed: ${group_speed.toFixed(2)} Gbit/s | Pps: ${group_pps} packets | `
-                        + `Received: ${group_packet_count} packets`);
-            }
-            
-        }
-        if (overall_speed > 0) {
-            countDown = 6;
-            if (gl_max_speed < overall_speed) gl_max_speed = overall_speed;
-            console.log('\n');
-            console.log(`Total Speed: ${overall_speed.toFixed(2)} Gbit/s | Pps: ${overall_pps} packets | Received: ${overall_packet_count} packets | `
-                + `Avg. packet size: ${Math.floor(((overall_speed*1e9)/overall_pps)/8)} bytes`);
-            gl_recieved_packets = overall_packet_count;
-        }
-        else if (countDown > 0) countDown--;
-
-        if (countDown == 2) {
-            console.log(`Test is over. Max speed: ${gl_max_speed.toFixed(2)} Gbit/s | Send packets: ${gl_send_packets == 0 ? 'No data' : gl_send_packets} | Received packets: ${gl_recieved_packets} | `
-                + `Loss percentage: ${gl_send_packets == 0 ? 'No data' : `${((1 - (gl_recieved_packets/gl_send_packets))*100).toFixed(2)}%`}`);
-            gl_send_packets = 0;
-            gl_recieved_packets = 0;
-            gl_max_speed = 0;
-            speedData.forEach((data) => {
-                data.speed = 0;
-                data.pps = 0;
-                data.overall = 0;
-            })
-        }        
-    }, 2000);
-
+    if (os.type() == 'Linux') {
+        const { pid } = process;
+        execSync(`taskset -cp ${0} ${pid}`);
+        console.log(`Main cluster with pid ${process.pid} running on Core ${0}`);
+    }
 } else {
     if (os.type() == 'Linux') {
         const { pid } = process;
@@ -198,4 +146,67 @@ if (cluster.isPrimary) {
         packetsReceived = 0;
         bytesReceived = 0;
     }, 1000);
+}
+
+function start(jsonData) {
+    let sensors = [];
+    let group_count = jsonData.groups.length;
+    for (let g = 0; g < group_count; g++) {
+        let group = jsonData.groups[g].sensors;
+
+        group.forEach((sensor) => {
+            sensors.push({group: g, ip: sensor.dst.split(':')[0], port: parseInt(sensor.dst.split(':')[1])});            
+        });
+    }
+
+    for (let i = 0; i < sensors.length; i++) {
+        _env.port = sensors[i].port;
+        _env.baseCPU = i;
+        _env.ip = sensors[i].ip | '0.0.0.0';
+        _env.mode = false;
+        const worker = cluster.fork(_env);
+        workers.push(worker);
+        speedData.push({port: _env.port, speed: 0, pps: 0, overall: 0, grp: sensors[i].group});
+        worker.on('message', (msg) => {
+            speedData[i].speed = msg.speed;
+            speedData[i].pps = msg.pps;
+            speedData[i].overall += msg.pps;
+        });
+    }
+
+    interval = setInterval(() => {
+        let overall_speed = 0;
+        let overall_packet_count = 0;
+        let overall_pps = 0;
+        
+        
+        for (let group_idx = 0; group_idx < group_count; group_idx++) {
+            let group_speed = 0;
+            let group_packet_count = 0;
+            let group_pps = 0;
+
+            speedData.forEach((group) => {
+                if (group.grp == group_idx) {
+                    group_speed += group.speed;
+                    group_packet_count += group.overall;
+                    group_pps += group.pps; 
+                }
+            });
+
+            if (group_idx == 0) console.log('\n\n');
+
+            overall_speed += group_speed;
+            overall_pps += group_pps;
+            overall_packet_count += group_packet_count;
+
+            console.log(`[GROUP:${group_idx}] Speed: ${group_speed.toFixed(2)} Gbit/s | Pps: ${group_pps} packets | `
+                    + `Received: ${group_packet_count} packets`);
+
+            if (gl_max_speed < overall_speed) gl_max_speed = overall_speed;
+            gl_recieved_packets = overall_packet_count;
+
+            console.log(`\nTotal Speed: ${overall_speed.toFixed(2)} Gbit/s | Pps: ${overall_pps} packets | Received: ${overall_packet_count} packets | `
+                + `Avg. packet size: ${Math.floor(((overall_speed*1e9)/overall_pps)/8)} bytes`);            
+        }      
+    }, 2000);
 }
